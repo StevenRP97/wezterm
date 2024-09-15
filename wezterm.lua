e18823207753defb8e81c6970d0a84c1a973f016
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-14 21:04:28 -0600

    Adding some Keybindings to navigate better through the config files for Neovim and Wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,24 @@
 local wezterm = require 'wezterm'
 local config = {}
+local act = wezterm.action
 
--- All my personal settings 
+if wezterm.config_builder then config = wezterm.config_builder() end
+
+-- These are the visual settings 
 config.color_scheme = 'WildCherry'
 
---config.font = wezterm.font 'DengXian'
+-- Keybindings
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL|ALT',
+    action = act.SendString('cd .config/wezterm\n'),
+  },
+  {
+    key = 'v',
+    mods = 'CTRL|ALT',
+    action = act.SendString('cd .config/nvim\n'),
+  }
 
-
+}
 return config
