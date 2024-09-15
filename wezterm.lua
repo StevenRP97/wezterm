eb3ca387996ccdabcf7351c6bacf8341bac07f98
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-14 20:24:35 -0600

    The very initial commit. Adding the file and a few initial configurations

---
+++ wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- All my personal settings 
+config.color_scheme = 'WildCherry'
+
+--config.font = wezterm.font 'DengXian'
+
+
+return config
